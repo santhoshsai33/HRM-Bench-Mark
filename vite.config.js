df8c8a08b231a7225b26f8f4b15b97552257b2bc
santhosh_s <santhoshs.saitech@gmail.com>
2026-04-09 15:11:06 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HRM-Bench-Mark/',
   plugins: [react()],
   server: {
-    base: '/HRM-Bench-Mark/',
     port: 5173,
     host: '192.168.1.151',
     open: true,
